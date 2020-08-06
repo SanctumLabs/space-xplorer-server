10a15ec42760616ff54285a6c84988b2f83b1984
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2020-08-07 00:05:05 +0300

    feat(datasource): launche api
    Add data source for Launch API fetching launch details from Space X Api

---
+++ src/schema.ts
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { gql } = require('apollo-server');
+
+module.exports = gql`
+  type Query {
+    launches: [Launch]!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  type Mutation {
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+    login(email: String): String
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+
+  type Rocket {
+    id: ID!
+    name: String!
+    type: String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    types: [Launch]!
+  }
+
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+`;
