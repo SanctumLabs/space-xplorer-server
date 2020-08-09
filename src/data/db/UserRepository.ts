1d9feff70def94deec073c71600acc49d8453f2b
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2020-08-09 19:02:01 +0300

    feat(mutations): mutations
    This handles mutations which allows users to signup, login, book trips &
    cancel them

--- src/data/db/UserRepository.ts
+++ src/data/db/UserRepository.ts
@@ -1,11 +1,12 @@
 import S3 from 'aws-sdk/clients/s3';
 import isEmail from 'isemail';
 import mime from 'mime';
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import { DataSource } from 'apollo-datasource';
 import db from './db';
 import { User, Trip } from '@prisma/client';
 import { storageBucket } from '@config';
+import logger from '@logger';
 
 export default class UserRepository extends DataSource {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -31,18 +32,24 @@ export default class UserRepository extends DataSource {
    * @param {string} email User's email address
    */
   async createUser(email: string): Promise<User> {
-    if (!email || isEmail.validate(email)) {
-      return null;
-    }
+    try {
+      if (!email || !isEmail.validate(email)) {
+        logger.error(`Email not provided or is invalid. Can't create user. Email: ${email}`);
+        return null;
+      }
 
-    const user = await db.user.create({
-      data: {
-        identifier: uuid.v4(),
-        email,
-        token: new Buffer(email).toString('base64'),
-      },
-    });
-    return user;
+      const user = await db.user.create({
+        data: {
+          identifier: uuidv4().toString(),
+          email,
+          token: new Buffer(email).toString('base64'),
+        },
+      });
+      logger.info(`Successfully created user ${email}`);
+      return user;
+    } catch (error) {
+      logger.error(`Failed to create user with error ${error}`);
+    }
   }
 
   /**
@@ -51,18 +58,25 @@ export default class UserRepository extends DataSource {
    * @param {string} email User's email address
    */
   async getUser(email?: string): Promise<User> {
-    const emailAddress = this.context && this.context.user ? this.context.user.email : email;
+    try {
+      const emailAddress = this.context && this.context.user ? this.context.user.email : email;
 
-    if (!emailAddress || isEmail.validate(emailAddress)) {
-      return null;
+      if (!emailAddress || !isEmail.validate(emailAddress)) {
+        logger.error(`Can't retrieve user. UserContext: ${this.context.user}, Email: ${email}`);
+        return null;
+      }
+
+      const user = await db.user.findOne({
+        where: {
+          email,
+        },
+      });
+      logger.info(`Retrieved user: ${user}`);
+      console.log(`Retreived user: ${JSON.stringify(user)}`);
+      return user;
+    } catch (error) {
+      logger.error(`Failed to get user with error ${error}`);
     }
-
-    const user = await db.user.findOne({
-      where: {
-        email,
-      },
-    });
-    return user;
   }
 
   /**
@@ -71,21 +85,27 @@ export default class UserRepository extends DataSource {
    */
   async bookTrips(launchIds: number[]): Promise<Trip[]> {
     const userId = this.context.user.identifier;
-
+    logger.info(`Booking trips ${launchIds} for user ${userId}`);
     if (!userId) {
+      logger.error(`Failed to book trips for user`);
       return;
     }
 
-    const bookedTrips: Trip[] = [];
+    try {
+      const bookedTrips: Trip[] = [];
 
-    for (const launchId of launchIds) {
-      const trip = await this.bookTrip(launchId);
-      if (trip) {
-        bookedTrips.push(trip);
+      for (const launchId of launchIds) {
+        const trip = await this.bookTrip(launchId);
+        if (trip) {
+          bookedTrips.push(trip);
+        }
       }
-    }
 
-    return bookedTrips;
+      logger.info(`Successfully booked trips ${launchIds} for User: ${userId}`);
+      return bookedTrips;
+    } catch (error) {
+      logger.error(`Failed to book trips for user ${userId} with error ${error}`);
+    }
   }
 
   /**
@@ -94,16 +114,25 @@ export default class UserRepository extends DataSource {
    */
   async bookTrip(launchId: number): Promise<Trip> {
     const userId = this.context.user.identifier;
+    logger.info(`Booking trip ${launchId} for User: ${userId} ...`);
 
-    const createdTrip = await db.trip.create({
-      data: {
-        identifier: uuid.v4(),
-        launchId,
-        user: userId,
-      },
-    });
+    try {
+      const createdTrip = await db.trip.create({
+        data: {
+          identifier: uuidv4(),
+          launchId,
+          user: {
+            connect: {
+              identifier: userId,
+            },
+          },
+        },
+      });
 
-    return createdTrip;
+      return createdTrip;
+    } catch (error) {
+      logger.error(`Failed to book a trip ${launchId} for user ${userId} with error ${error}`);
+    }
   }
 
   /**
@@ -112,27 +141,35 @@ export default class UserRepository extends DataSource {
    */
   async cancelTrip(launchId: number): Promise<Trip> {
     const userId = this.context.user.identifier;
+    logger.info(`Cancelling trip ${launchId} for User: ${userId} ...`);
 
     if (!userId) {
+      logger.info(`Failed to cancel trip ${launchId} for User: ${userId} ...`);
       return null;
     }
 
-    const user = await db.user.findOne({ where: { identifier: userId } });
+    try {
+      const user = await db.user.findOne({ where: { identifier: userId } });
 
-    const deletedTrip = await db.trip.delete({
-      include: {
-        user: {
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          select: { id: user.id },
+      const deletedTrip = await db.trip.delete({
+        include: {
+          user: {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            select: { id: user.id },
+          },
         },
-      },
-      where: {
-        launchId,
-      },
-    });
+        where: {
+          launchId,
+        },
+      });
 
-    return deletedTrip;
+      logger.info(`Successfully cancelled trip ${launchId} for User: ${userId} ...`);
+
+      return deletedTrip;
+    } catch (error) {
+      logger.error(`Failed to cancel trip for user with error ${error}`);
+    }
   }
 
   /**
@@ -140,34 +177,47 @@ export default class UserRepository extends DataSource {
    */
   async getLaunchIdsByUser(): Promise<number[]> {
     const userId = this.context.user.identifier;
-    const foundTrips = await db.trip.findMany({
-      where: {
-        user: {
-          identifier: userId,
-        },
-      },
-    });
+    logger.info(`Getting launchIds for User: ${userId} ...`);
 
-    return foundTrips.map((trip) => trip.launchId);
+    try {
+      const foundTrips = await db.trip.findMany({
+        where: {
+          user: {
+            identifier: userId,
+          },
+        },
+      });
+
+      logger.debug(`Found trips ${foundTrips} for User: ${userId} ...`);
+
+      return foundTrips.map((trip) => trip.launchId);
+    } catch (error) {
+      logger.error(`Failed to get launch IDs for user ${userId} with error ${error}`);
+    }
   }
 
   /**
-   *
+   *Checks if the current trip is booked
    * @param {number} launchId Launch ID
    */
   async isBookedOnLaunch(launchId: number): Promise<boolean> {
     if (!this.context || !this.context.user) return false;
     const userId = this.context.user.identifier;
-    const foundTrips = await db.trip.findMany({
-      where: {
-        launchId,
-        user: {
-          identifier: userId,
-        },
-      },
-    });
 
-    return foundTrips && foundTrips.length > 0;
+    try {
+      const foundTrips = await db.trip.findMany({
+        where: {
+          launchId,
+          user: {
+            identifier: userId,
+          },
+        },
+      });
+
+      return foundTrips && foundTrips.length > 0;
+    } catch (error) {
+      logger.error(`Failed to check if Trip ${launchId} is booked for user ${userId}`);
+    }
   }
 
   /**
@@ -180,36 +230,40 @@ export default class UserRepository extends DataSource {
     const userId = this.context.user.identifier;
     if (!userId) return;
 
-    // Create new S3 client instance
-    const s3 = new S3();
+    try {
+      // Create new S3 client instance
+      const s3 = new S3();
 
-    /**
-     * Destructure mimetype and stream creator from provided file and generate
-     * a unique filename for the upload
-     */
-    const { createReadStream, mimetype } = await file;
-    const filename = uuid.v4() + '.' + mime.getExtension(mimetype);
+      /**
+       * Destructure mimetype and stream creator from provided file and generate
+       * a unique filename for the upload
+       */
+      const { createReadStream, mimetype } = await file;
+      const filename = uuidv4() + '.' + mime.getExtension(mimetype);
 
-    // Upload the file to an S3 bucket using the createReadStream
-    await s3
-      .upload({
-        ACL: 'public-read', // This will make the file publicly available
-        Body: createReadStream(),
-        Bucket: storageBucket,
-        Key: filename,
-        ContentType: mimetype,
-      })
-      .promise();
+      // Upload the file to an S3 bucket using the createReadStream
+      await s3
+        .upload({
+          ACL: 'public-read', // This will make the file publicly available
+          Body: createReadStream(),
+          Bucket: storageBucket,
+          Key: filename,
+          ContentType: mimetype,
+        })
+        .promise();
 
-    // Save the profile image URL in the DB and return the updated user
-    const updatedUser = await db.user.update({
-      where: {
-        identifier: userId,
-      },
-      data: {
-        profileImage: `https://${storageBucket}.s3.us-west-2.amazonaws.com/${filename}`,
-      },
-    });
-    return updatedUser;
+      // Save the profile image URL in the DB and return the updated user
+      const updatedUser = await db.user.update({
+        where: {
+          identifier: userId,
+        },
+        data: {
+          profileImage: `https://${storageBucket}.s3.us-west-2.amazonaws.com/${filename}`,
+        },
+      });
+      return updatedUser;
+    } catch (error) {
+      logger.error(`Failed to upload image for user ${userId} with error ${error}`);
+    }
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,54 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 require('dotenv').config();
-const typeDefs = require('./schema');
+
+import { ApolloServer } from 'apollo-server';
 
 import logger from '@logger';
 import LaunchApi from '@launchApi';
 import UserRepo from '@userRepo';
-import queryResolver from '@resolvers/queryResolver';
-import missionResolver from '@resolvers/missionResolver';
-import userResolver from '@resolvers/userResolver';
-import launchResolver from '@resolvers/launchResolver';
-import mutationResolver from '@resolvers/mutationResolver';
-import { ApolloServer } from 'apollo-server';
+import queryResolver from '@queryResolver';
+import missionResolver from '@missionResolver';
+import userResolver from '@userResolver';
+import launchResolver from '@launchResolver';
+import mutationResolver from '@mutationResolver';
+import typeDefs from '@gql/schema';
+import isEmail from 'isemail';
+import { environment } from '@config';
 
 process.on('uncaughtException', (e) => {
   logger.error(`UncaughtException ${e}`);
 });
 
+const userRepo = new UserRepo();
+
+// set up any dataSources our resolvers need
+const dataSources = () => ({
+  launchApi: new LaunchApi(),
+  userRepo: userRepo,
+});
+
+// the function that sets up the global context for each resolver, using the req
+const context = async ({ req }: any) => {
+  // simple auth check on every request
+  const auth = (req.headers && req.headers.authorization) || '';
+  const email = new Buffer(auth, 'base64').toString('ascii');
+
+  // if the email isn't formatted validly, return null for user
+  if (!isEmail.validate(email)) return { user: null };
+
+  // find a user by their email
+  const user = await userRepo.getUser(email);
+  return { user };
+};
+
 const server = new ApolloServer({
   typeDefs,
   resolvers: [queryResolver, missionResolver, userResolver, launchResolver, mutationResolver],
-  dataSources: () => ({
-    launchApi: new LaunchApi(),
-    userRepo: new UserRepo(),
-  }),
+  dataSources,
+  context,
+  playground: environment !== 'production',
+  introspection: environment !== 'production',
 });
 
 server.listen().then(({ url }: any) => {
